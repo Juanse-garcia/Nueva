61ad5dfbdf9da9ee39be480b2d56b9bb036d7aff
Juanse-garcia <66497252+Juanse-garcia@users.noreply.github.com>
2020-06-05 21:20:03 -0500

    nuevocod

---
+++ Codigo.R
@@ -0,0 +1,6 @@
+library(readxl)
+regresion <- read_excel("regresion.xlsx", 
+                          col_types = c("numeric", "numeric", "numeric", 
+                          "numeric", "numeric", "numeric", 
+                          "numeric", "numeric", "text"))
+hist(regresion$pm2_5)
